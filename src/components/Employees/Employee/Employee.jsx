b9b097bed29624bb69fca57093ee5257c1b09f47
NamDo8467 <huynhnamdo@gmail.com>
2022-11-25 17:06:50 -0500

    change useEffect dependecy

--- src/components/Employees/Employee/Employee.jsx
+++ src/components/Employees/Employee/Employee.jsx
@@ -13,9 +13,10 @@ function Employee() {
 				setEmployee(response.data)
 			})
 			.catch(error => {
+				console.log(error)
 				setEmployee({})
 			})
-	}, [])
+	}, [URL])
 	return (
 		<div className='border border-green-500 h-4/5 relative'>
 			<div className='employee-detail grid grid-cols-2 w-1/3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-purple-300 py-2'>
